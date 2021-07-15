b352e3c53970fbd3d799d57c3fe52862a4c6e978
karinlmarques <karinlmarques@gmail.com>
2021-07-15 12:37:34 -0300

    adicionando validação dos inputs

--- js/script.js
+++ js/script.js
@@ -1,9 +1,21 @@
 const form = document.getElementById("form");
 
+
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   let nome = document.getElementById("nome").value;
+  if(document.getElementById("nome").value == ""){
+    alert('Por favor, preencha o campo nome');
+    document.getElementById("nome").focus();
+    return false;
+  }
   let email = document.getElementById("email").value;
+  if(document.getElementById("email").value == ""){
+    alert('Por favor, preencha o campo e-mail');
+    document.getElementById("email").focus();
+    return false;
+  }
   let data = {
     nome,
     email,
